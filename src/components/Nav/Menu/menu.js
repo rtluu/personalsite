bb0f56c9ae0fd297b87e4a28854b29db44a730a3
Ryan Luu <ryan@vumble.com>
2020-04-26 15:11:52 -0400

    Folders in Side Menu

--- src/components/Nav/Menu/menu.js
+++ src/components/Nav/Menu/menu.js
@@ -2,8 +2,8 @@ import React from "react";
 import { useLocation } from "@reach/router"
 import { Link } from "gatsby";
 import "./menu.scss";
+import FolderIcon from "../../../icons/folder-icon.inline.svg";
 import HomeIcon from "../../../icons/home-icon.inline.svg";
-import DocIcon from "../../../icons/doc-icon.inline.svg";
 import Tooltip from "../../Tooltip/tooltip";
 
 const Menu = () => {
@@ -19,36 +19,47 @@ const Menu = () => {
                     </button>
                 </Link>
             </div>
-            <ul className="menu-option-set">
-                <h4 className="bold">Product</h4>
-                <li className={location.pathname === "/washingtonpost/" && ('active')}>
-                    <Link to="/washingtonpost/"><DocIcon /><h5>Washington Post</h5></Link>
-                </li>
-                <li className={location.pathname === "/adhoc/" && ('active')}>
-                    <Link to="/adhoc/"><DocIcon /><h5>Ad Hoc</h5></Link>
-                </li>
-                <li className={location.pathname === "/vumble/" && ('active')}>
-                    <Link to="/vumble/"><DocIcon /><h5>Vumble</h5></Link>
-                </li>
-            </ul>
-            <ul className="menu-option-set">
-                <h4 className="bold">Development</h4>
-                <li className={location.pathname === "/openrecord/" && ('active')}>
-                    <Link to="/openrecord/"><DocIcon /><h5>OpenRecord</h5></Link>
-                </li>
-                <li className={location.pathname === "/neat/" && ('active')}>
-                    <Link to="/neat/"><DocIcon /><h5>Neat</h5></Link>
-                </li>
-                <li className={location.pathname === "/ourluubeginning/" && ('active')}>
-                    <Link to="/ourluubeginning/"><DocIcon /><h5>OurLuuBeginning</h5></Link>
-                </li>
-                <li className={location.pathname === "/fastrope/" && ('active')}>
-                    <Link to="/fastrope/"><DocIcon /><h5>Fastrope Labs</h5></Link>
-                </li>
-                <li className={location.pathname === "/humblevc/" && ('active')}>
-                    <Link to="/humblevc/"><DocIcon /><h5>Humble VC</h5></Link>
-                </li>
-            </ul>
+            <div className="folder-group">
+                <div className="folder-title">
+                    <FolderIcon />
+                    <h4 className="bold">Product</h4>
+                </div>
+                <ul className="folder-list">
+                    <li className={location.pathname === "/washingtonpost/" && ('active')}>
+                        <Link to="/washingtonpost/"><h5>Washington Post</h5></Link>
+                    </li>
+                    <li className={location.pathname === "/adhoc/" && ('active')}>
+                        <Link to="/adhoc/"><h5>Ad Hoc</h5></Link>
+                    </li>
+                    <li className={location.pathname === "/vumble/" && ('active')}>
+                        <Link to="/vumble/"><h5>Vumble</h5></Link>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="folder-group">
+                <div className="folder-title">
+                    <FolderIcon />
+                    <h4 className="bold">Development</h4>
+                </div>
+                <ul className="folder-list">
+                    <li className={location.pathname === "/openrecord/" && ('active')}>
+                        <Link to="/openrecord/"><h5>OpenRecord</h5></Link>
+                    </li>
+                    <li className={location.pathname === "/neat/" && ('active')}>
+                        <Link to="/neat/"><h5>Neat</h5></Link>
+                    </li>
+                    <li className={location.pathname === "/ourluubeginning/" && ('active')}>
+                        <Link to="/ourluubeginning/"><h5>OurLuuBeginning</h5></Link>
+                    </li>
+                    <li className={location.pathname === "/fastrope/" && ('active')}>
+                        <Link to="/fastrope/"><h5>Fastrope Labs</h5></Link>
+                    </li>
+                    <li className={location.pathname === "/humblevc/" && ('active')}>
+                        <Link to="/humblevc/"><h5>Humble VC</h5></Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
